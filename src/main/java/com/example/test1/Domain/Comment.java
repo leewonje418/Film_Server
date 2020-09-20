2eadf53491672950917df769a131029d8edd0105
leewonje418 <kitti0418@naver.com>
2020-09-20 13:00:51 +0900

    Comment Start

--- src/main/java/com/example/test1/Domain/Comment.java
+++ src/main/java/com/example/test1/Domain/Comment.java
@@ -1,5 +1,4 @@
 package com.example.test1.Domain;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -8,25 +7,17 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class User {
+public class Comment {
     @Id
     @GeneratedValue
     private Long id;
-    private String user_id;
-    private String nickName;
-    private String userName;
-    private String phoneNumber;
-    private LocalDate birthDate;
-    private int gender;
-    private String email;
-    private String password;
-    private String storedPath;
+    private Long User_id;
+    private String comment;
     @CreationTimestamp
     private LocalDateTime createdAt;
     @UpdateTimestamp
