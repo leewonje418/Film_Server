930009c2e34a2716359425f4223f2cc9fe5fce9c
leewonje418 <kitti0418@naver.com>
2020-09-20 14:48:38 +0900

    user_api_test_result

---
+++ src/main/java/com/example/test1/Service/CommentService.java
@@ -0,0 +1,14 @@
+package com.example.test1.Service;
+
+import com.example.test1.Domain.Comment;
+import com.example.test1.Protocol.CommentUserProtocol;
+
+import java.util.List;
+
+public interface CommentService {
+    CommentUserProtocol add(Comment comment);
+    boolean remove(Long id);
+    CommentUserProtocol update(Long id, Comment comment);
+    CommentUserProtocol view(Long id);
+    List<CommentUserProtocol> listAllComments();
+}
