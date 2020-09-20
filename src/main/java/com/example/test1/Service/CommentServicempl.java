736bd8e8a1c19e574ae157434d0ae64bb59e53d3
leewonje418 <kitti0418@naver.com>
2020-09-20 15:51:23 +0900

    service,protocol,service_Done

--- src/main/java/com/example/test1/Service/CommentServicempl.java
+++ src/main/java/com/example/test1/Service/CommentServicempl.java
@@ -1,13 +1,16 @@
 package com.example.test1.Service;
 
 import com.example.test1.Domain.Comment;
+import com.example.test1.Domain.User;
 import com.example.test1.Protocol.CommentUserProtocol;
 import com.example.test1.Repository.CommentRepository;
 import com.example.test1.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommentServicempl implements CommentService {
@@ -28,21 +31,44 @@ public class CommentServicempl implements CommentService {
 
     @Override
     public boolean remove(Long id) {
-        return false;
+        return true;
     }
 
     @Override
     public CommentUserProtocol update(Long id, Comment comment) {
+        Optional<User> user = userRepository.findById(comment.getUser_id());
+        if(user.isPresent()) {
+            return commentRepository.findById(id)
+                    .map(found -> {
+                        found.setComment(Optional.ofNullable(comment.getComment()).orElse(found.getComment()));
+                        commentRepository.save(found);
+                        return new CommentUserProtocol(found, user.get().getUserName());
+                    })
+                    .orElse(null);
+        }
         return null;
     }
 
     @Override
     public CommentUserProtocol view(Long id) {
-        return null;
+        return commentRepository.findById(id)
+                .map(found -> {
+                    Optional<User> user = userRepository.findById(found.getUser_id());
+                    String username = user.isPresent() ? user.get().getUserName() : null;
+                    return new CommentUserProtocol(found, username);
+                })
+                .orElse(null);
     }
 
     @Override
     public List<CommentUserProtocol> listAllComments() {
-        return null;
+        List<Comment> commentList = commentRepository.findAll();
+        List<CommentUserProtocol> cupList = new ArrayList<>();
+        commentList.forEach(comment -> {
+            Optional<User> found = userRepository.findById(comment.getUser_id());
+            String username = found.isPresent() ? found.get().getUserName() : null;
+            cupList.add(new CommentUserProtocol(comment, username));
+        });
+        return cupList;
     }
 }
