7c22362ee944d895565ed7420e8c12cfc1436dde
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-10-14 19:35:55 +0900

    Update CommentController.java

--- src/main/java/com/example/test1/Controller/CommentController.java
+++ src/main/java/com/example/test1/Controller/CommentController.java
@@ -1,4 +1,37 @@
 package com.example.test1.Controller;
 
+import com.example.test1.Domain.Comment;
+import com.example.test1.Domain.User;
+import com.example.test1.Protocol.CommentUserProtocol;
+import com.example.test1.Service.CommentService;
+import com.example.test1.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CommentController {
+    @Autowired
+    private CommentService commentService;
+    @PostMapping("/comment/add")
+    public CommentUserProtocol add(@RequestBody Comment comment) {
+        return commentService.add(comment);
+    }
+    @PutMapping("/comment/update/{id}")
+    public CommentUserProtocol update(@PathVariable Long id, @RequestBody Comment comment) {
+        return commentService.update(id, comment);
+    }
+    @DeleteMapping("/comment/remove/{id}")
+    public boolean remove(@PathVariable Long id) {
+        return commentService.remove(id);
+    }
+    @GetMapping("/comment/view/{id}")
+    public CommentUserProtocol view(@PathVariable Long id) {
+        return commentService.view(id);
+    }
+    @GetMapping("/comment/list")
+    public List<CommentUserProtocol> listAllComments() {
+        return commentService.listAllComments();
+    }
 }
