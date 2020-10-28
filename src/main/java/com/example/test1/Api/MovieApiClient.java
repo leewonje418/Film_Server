5cbe24960b4f00ddcbabc468bd0112fc8c43a3a8
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-10-28 20:38:26 +0900

    Update MovieApiClient.java

--- src/main/java/com/example/test1/Api/MovieApiClient.java
+++ src/main/java/com/example/test1/Api/MovieApiClient.java
@@ -17,7 +17,7 @@ public class MovieApiClient {
     private final String CLIENT_SECRET = "luc9N7726n";
     private final String api = "https://openapi.naver.com/v1/search/movie.json?query={keyword}";
     public MoviesDto requestMovie(String keyword) {
-        final HttpHeaders headers = new HttpHeaders(); // 헤더에 key들을 담아준다.
+        final HttpHeaders headers = new HttpHeaders();
         headers.set("X-Naver-Client-Id", CLIENT_ID);
         headers.set("X-Naver-Client-Secret", CLIENT_SECRET);
         final HttpEntity<String> entity = new HttpEntity<>(headers);
