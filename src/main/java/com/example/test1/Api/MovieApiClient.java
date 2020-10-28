9d28c527925e0390aef3bc7dd858521805cc3ade
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-10-28 18:06:12 +0900

    Create MovieApiClient.java

---
+++ src/main/java/com/example/test1/Api/MovieApiClient.java
@@ -0,0 +1,26 @@
+package com.example.test1.Api;
+
+import com.example.test1.Domain.MoviesDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@RequiredArgsConstructor
+@Service
+public class MovieApiClient {
+    private RestTemplate restTemplate = new RestTemplate();
+    private final String CLIENT_ID = "BJQYpubXwcJYe46wRXsl";
+    private final String CLIENT_SECRET = "luc9N7726n";
+    private final String api = "https://openapi.naver.com/v1/search/movie.json?query={keyword}";
+    public MoviesDto requestMovie(String keyword) {
+        final HttpHeaders headers = new HttpHeaders(); // 헤더에 key들을 담아준다.
+        headers.set("X-Naver-Client-Id", CLIENT_ID);
+        headers.set("X-Naver-Client-Secret", CLIENT_SECRET);
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
+        return restTemplate.exchange(api, HttpMethod.GET, entity, MoviesDto.class, keyword).getBody();
+    }
+}
