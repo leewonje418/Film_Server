59b2ec5127014f770170892cd4dc6aed62bdfc12
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-04 18:11:53 +0900

    [FIX] 일별 박스오피스 완료

---
+++ src/main/java/com/example/test1/Controller/DailyBoxOfficeController.java
@@ -0,0 +1,20 @@
+package com.example.test1.Controller;
+
+import com.example.test1.Api.DailyBoxOfficeClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class DailyBoxOfficeController {
+    private final DailyBoxOfficeClient dailyBoxOfficeClient;
+    @GetMapping("/api/v1/dailyboxoffice/{keyword}")
+    public String get(@PathVariable String keyword) throws JsonProcessingException {
+        return dailyBoxOfficeClient.requestDailyBoxOffice(keyword);
+    }
+}
