925258db0e38a66455058e40ed2cdbb24a1e251e
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-10 17:50:00 +0900

    [UPDATE] 주간 박스오피스 컨트롤러 업데이트

--- src/main/java/com/example/test1/Controller/WeeklyBoxOfficeController.java
+++ src/main/java/com/example/test1/Controller/WeeklyBoxOfficeController.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class WeeklyBoxOfficeController {
     private final WeeklyBoxOfficeClient weeklyboxofficeclient;
     @GetMapping("/api/v1/weeklyboxoffice/{keyword}")
-    public String get(@PathVariable String keyword) throws Exception {
+    public Map get(@PathVariable String keyword) throws Exception {
         return weeklyboxofficeclient.requestWeeklyBoxOffice(keyword);
     }
 }
