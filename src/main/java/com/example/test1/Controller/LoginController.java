b2398ee96c4ee11660f1fda1c3b171c4953c79cd
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-14 23:36:14 +0900

    [UPDATE] 로그인 컨트롤러 수정

--- src/main/java/com/example/test1/Controller/LoginController.java
+++ src/main/java/com/example/test1/Controller/LoginController.java
@@ -1,4 +1,23 @@
 package com.example.test1.Controller;
 
+import com.example.test1.Domain.User;
+import com.example.test1.Service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@CrossOrigin(origins = "*")
+@RestController
+@SessionAttributes("user")
 public class LoginController {
+    @Autowired
+    private LoginService loginService;
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value="/login", produces="text/xml;charset=UTF-8", method=RequestMethod.POST)
+    public void login(@RequestBody User user, HttpServletRequest req, HttpServletResponse res) throws IOException {
+        loginService.login(user.getEmail(), user.getPassword(), req, res);
+    }
 }
