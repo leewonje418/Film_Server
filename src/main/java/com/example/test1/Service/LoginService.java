7d22197bb941b62edb4522419bcc48b7ec53e368
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-16 23:05:04 +0900

    Update LoginService.java

--- src/main/java/com/example/test1/Service/LoginService.java
+++ src/main/java/com/example/test1/Service/LoginService.java
@@ -1,8 +1,10 @@
 package com.example.test1.Service;
 
+import org.springframework.ui.Model;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface LoginService {
-    boolean login(String email, String password, HttpServletRequest req, HttpServletResponse res);
+    boolean login(String email, String password, Model model, HttpServletRequest req, HttpServletResponse res);
 }
