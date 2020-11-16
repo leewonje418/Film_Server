112c1f6ef8effc823ec2a813e757dbfe18581caf
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-16 23:05:02 +0900

    Update LoginController.java

--- src/main/java/com/example/test1/Controller/LoginController.java
+++ src/main/java/com/example/test1/Controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.test1.Controller;
 import com.example.test1.Domain.User;
 import com.example.test1.Service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +18,7 @@ public class LoginController {
     private LoginService loginService;
     @CrossOrigin(origins = "*")
     @RequestMapping(value="/login", produces="text/xml;charset=UTF-8", method=RequestMethod.POST)
-    public void login(@RequestBody User user, HttpServletRequest req, HttpServletResponse res) throws IOException {
-        loginService.login(user.getEmail(), user.getPassword(), req, res);
+    public void login(@RequestBody User user, Model model, HttpServletRequest req, HttpServletResponse res) throws IOException {
+        loginService.login(user.getEmail(), user.getPassword(), model, req, res);
     }
 }
