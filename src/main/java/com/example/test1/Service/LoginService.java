a96fd1fd44df248f24f03db3f85077feaa0013e3
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-17 19:29:59 +0900

    [UPDATE] 로그인 최종 수정

--- src/main/java/com/example/test1/Service/LoginService.java
+++ src/main/java/com/example/test1/Service/LoginService.java
@@ -1,10 +1,11 @@
 package com.example.test1.Service;
 
+import com.example.test1.Domain.User;
 import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface LoginService {
-    boolean login(String email, String password, Model model, HttpServletRequest req, HttpServletResponse res);
+    User login(String email, String password, Model model, HttpServletRequest req, HttpServletResponse res);
 }
