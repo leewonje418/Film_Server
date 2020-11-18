3877e6b596e79459b53b46e9d04aba83252a1a0a
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-18 22:10:53 +0900

    [UPDATE] 함수 추가

--- src/main/java/com/example/test1/Repository/CommentRepository.java
+++ src/main/java/com/example/test1/Repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.example.test1.Repository;
 import com.example.test1.Domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository  extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByMovieNameAndDirector(String movieName, String director);
 }
