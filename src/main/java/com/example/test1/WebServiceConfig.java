9a0ffc2dd3a5d1bc9de8c859ff377943740ae507
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-18 19:24:18 +0900

    Update WebServiceConfig.java

--- src/main/java/com/example/test1/WebServiceConfig.java
+++ src/main/java/com/example/test1/WebServiceConfig.java
@@ -13,7 +13,7 @@ public class WebServiceConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/comment/**", "/user/update/**", "/user/delete/**"); // 이 경로로 REQUEST 오면 Interceptor 발동
+                .addPathPatterns("/comment/**", "/user/update/**", "/user/delete/**", "/logout"); // 이 경로로 REQUEST 오면 Interceptor 발동
     }
     @Override
     public void addCorsMappings(CorsRegistry registry) { // CORS 처리라서 냅두세요
